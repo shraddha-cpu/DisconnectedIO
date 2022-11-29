260e66ea8d9feafb84bf724d9b096b0d5cf02805
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-11-29 14:18:41 +0530

    Add project files.

---
+++ DisconnectedIO/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DisconnectedIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.EmpId = new System.Windows.Forms.Label();
+            this.EmpName = new System.Windows.Forms.Label();
+            this.EmpSalary = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(85, 213);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(210, 213);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(335, 213);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(608, 256);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(75, 23);
+            this.btnShowAll.TabIndex = 3;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(461, 60);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // EmpId
+            // 
+            this.EmpId.AutoSize = true;
+            this.EmpId.Location = new System.Drawing.Point(108, 60);
+            this.EmpId.Name = "EmpId";
+            this.EmpId.Size = new System.Drawing.Size(68, 13);
+            this.EmpId.TabIndex = 5;
+            this.EmpId.Text = "Enter Emp Id";
+            // 
+            // EmpName
+            // 
+            this.EmpName.AutoSize = true;
+            this.EmpName.Location = new System.Drawing.Point(108, 111);
+            this.EmpName.Name = "EmpName";
+            this.EmpName.Size = new System.Drawing.Size(87, 13);
+            this.EmpName.TabIndex = 6;
+            this.EmpName.Text = "Enter Emp Name";
+            // 
+            // EmpSalary
+            // 
+            this.EmpSalary.AutoSize = true;
+            this.EmpSalary.Location = new System.Drawing.Point(108, 163);
+            this.EmpSalary.Name = "EmpSalary";
+            this.EmpSalary.Size = new System.Drawing.Size(88, 13);
+            this.EmpSalary.TabIndex = 7;
+            this.EmpSalary.Text = "Enter Emp Salary";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(210, 53);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 8;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(210, 111);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(210, 160);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 10;
+            // 
+            // txtShow
+            // 
+            this.txtShow.Location = new System.Drawing.Point(572, 40);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(172, 140);
+            this.txtShow.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.EmpSalary);
+            this.Controls.Add(this.EmpName);
+            this.Controls.Add(this.EmpId);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Dissconnectedio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label EmpId;
+        private System.Windows.Forms.Label EmpName;
+        private System.Windows.Forms.Label EmpSalary;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtShow;
+    }
+}
+
